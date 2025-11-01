438837cf6f0ed58e052d2147372402da6291423a
Rishita Saha <rishitasaha291@gmail.com>
2025-11-02 01:47:01 +0530

    0125-VALID-PALINDROME Stats: Time: 14 ms (28.36%), Space: 46.1 MB (7.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.replaceAll("[^a-zA-Z0-9]", "");
-        String st = str.toLowerCase();
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();;
         int l=0;
-        int r = st.length()-1;
+        int r = str.length()-1;
         while(l<=r){
-            if(st.charAt(l) != st.charAt(r)){
+            if(str.charAt(l) != str.charAt(r)){
                 return false;
             }
             l++;
