dd152c7671be72331a11f88c14959d5a48cddd3b
Rishita Saha <rishitasaha291@gmail.com>
2026-02-10 21:58:24 +0530

    0123-BEST-TIME-TO-BUY-AND-SELL-STOCK-III Stats: Time: 1 ms (99.45%), Space: 76 MB (71.94%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int b1,b2,s1,s2,n;
+        n = prices.length;
+        b1 = b2 = Integer.MIN_VALUE;
+        s1 = s2 = 0;
+        for(int p : prices){
+            b1 = Math.max(b1,-p);
+            s1 = Math.max(s1,b1+p);
+            b2 = Math.max(b2,s1-p);
+            s2 = Math.max(s2,b2+p);
+        }
+
+        return s2;
+    }
+}
