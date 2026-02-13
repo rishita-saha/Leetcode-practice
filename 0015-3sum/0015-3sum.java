62701c4ba987652fcb02dc2136578a1751ae3a82
Rishita Saha <rishitasaha291@gmail.com>
2026-02-14 02:39:01 +0530

    0015-3SUM Stats: Time: 29 ms (91.71%), Space: 59.2 MB (74.33%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,8 +1,9 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        
-        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        //Set<List<Integer>> s = new HashSet<>();
         //Brute force
         // for(int i=0;i<n;i++){
         //     for(int j=i+1;j<n;j++){
@@ -18,20 +19,47 @@ class Solution {
         // }
 
         //Better approach 
+        // for(int i=0;i<n;i++){
+        //     Set<Integer> h =new HashSet<>();
+        //     for(int j=i+1;j<n;j++){
+        //         int td = -(nums[i] + nums[j]);
+        //         if(h.contains(td)){
+        //             List<Integer> list = Arrays.asList(nums[i],nums[j],td);
+        //             Collections.sort(list);
+        //             s.add(list); 
+        //         }
+        //         h.add(nums[j]);
+        //     }
+        // }
+
+        //Optimal
+        Arrays.sort(nums);
         for(int i=0;i<n;i++){
-            Set<Integer> h =new HashSet<>();
-            for(int j=i+1;j<n;j++){
-                int td = -(nums[i] + nums[j]);
-                if(h.contains(td)){
-                    List<Integer> list = Arrays.asList(nums[i],nums[j],td);
-                    Collections.sort(list);
-                    s.add(list); 
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0)
+                    j++;
+                else if(sum>0)
+                    k--;
+                else{
+                    List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
+                    res.add(list);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1])
+                        j++;
+                    while(j<k && nums[k] == nums[k+1])
+                        k--;
                 }
-                h.add(nums[j]);
+
             }
         }
 
-        List<List<Integer>> res = new ArrayList<>(s);
+        //List<List<Integer>> res = new ArrayList<>(s);
         return res;
     }
 }
