b33d9d1679d8a1ad37c085a2c0432295864f52d9
Rishita Saha <rishitasaha291@gmail.com>
2026-03-06 19:06:07 +0530

    1910-CHECK-IF-BINARY-STRING-HAS-AT-MOST-ONE-SEGMENT-OF-ONES Stats: Time: 0 ms (100%), Space: 42.9 MB (50.94%) - LeetHub

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/1910-check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,9 +1,15 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i-1)=='0' && s.charAt(i)=='1')
-                return false;
-        }
-        return true;
+        // for(int i=1;i<s.length();i++){
+        //     if(s.charAt(i-1)=='0' && s.charAt(i)=='1')
+        //         return false;
+        // }
+        // return true;
+        int i=0,n=s.length();
+        while(i<n && s.charAt(i)=='1')
+            i++;
+        while(i<n && s.charAt(i)=='0')
+            i++;
+        return i>=n ? true : false;
     }
 }
